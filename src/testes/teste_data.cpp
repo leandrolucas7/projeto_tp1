ca8e19f5d0eedcaf0dcb07970387ef35dcf33ac5
Rafael-Sapienza <rafaelsapienzapinheiro@gmail.com>
2024-11-29 19:03:49 -0300

    remendos2

--- src/testes/teste_data.cpp
+++ src/testes/teste_data.cpp
@@ -4,6 +4,6 @@
 void TesteData::set_valores()
 {
     dominio = new Data();
-    valores_validos = {"16-12-24","29-09-16","31-01-23","00-05-00"};
-    valores_invalidos = {"31-04-10","12-00-90","12/07/13"};
+    valores_validos = {"16-12-24","29-09-16","31-01-23","01-05-00"};
+    valores_invalidos = {"31-04-10","12-00-90","12/07/13","00-05-00"};
 }
