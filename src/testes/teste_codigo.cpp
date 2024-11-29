e67c7ca9e735c68e223ef3e8cfc43c403f864c66
Leandro <leandrolucas7@hotmail.com>
2024-11-29 15:14:29 -0300

    correção do teste codigo / novos itens no makefile

--- src/testes/teste_codigo.cpp
+++ src/testes/teste_codigo.cpp
@@ -1,11 +1,10 @@
 #include "../../include/dominios/DOMINIOS.hpp"
 #include "../../include/testes/TESTES.hpp"
-#include "teste_codigo.hpp"
 #include <string>
 
 void TesteCodigo::set_valores()
 {
     dominio = new Codigo;
-    valores_invalidos = {"1234567", "a$bcde", };
-
+    valores_validos = {"A1b2C3", "aAbBcC", "123456", "unb100"};
+    valores_invalidos = {"1234567", "a$bcde", "abc", "df.23ab", "df1$$$"};
 }
