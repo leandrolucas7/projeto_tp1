3504695614c55353bc699fcf76a19854bee28c0b
Rafael-Sapienza <rafaelsapienzapinheiro@gmail.com>
2024-11-29 19:25:49 -0300

    Pasta de testes foi realocada

---
+++ src/dominios/testes/teste_avaliacao.cpp
@@ -0,0 +1,9 @@
+#include "../../include/dominios/DOMINIOS.hpp"
+#include "../../include/dominios/testes/TESTES.hpp"
+
+void TesteAvaliacao::set_valores()
+{
+    dominio = new Avaliacao();
+    valores_validos = {0, 1, 2, 3, 4, 5};
+    valores_invalidos = {-100, -1, 6, 100};
+}
