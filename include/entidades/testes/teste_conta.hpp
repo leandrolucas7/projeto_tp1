4dc24e16cc8175d497d9d49b447925f7fca0d2bb
Rafael-Sapienza <rafaelsapienzapinheiro@gmail.com>
2024-11-29 20:58:59 -0300

    primeiro teste de entidades criado

---
+++ include/entidades/testes/teste_conta.hpp
@@ -0,0 +1,21 @@
+#include "../ENTIDADES.hpp"
+#include <vector>
+#include <string>
+using namespace std;
+
+
+class TesteConta 
+{
+    private:
+        const static vector<string> VALORES_VALIDOS_CODIGO;
+        const static vector<string> VALORES_VALIDOS_SENHA;
+        Conta *conta;
+        int estado;
+        void set_up();
+        void tear_down();
+        void testar_cenario();
+    public:
+        const static int SUCESSO = 1;
+        const static int FALHA = 0;
+        int run();
+};
