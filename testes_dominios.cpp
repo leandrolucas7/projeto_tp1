146fa7dfbead3a048723c919aebd290234a217ee
Rafael-Sapienza <rafaelsapienzapinheiro@gmail.com>
2024-11-29 18:47:21 -0300

    teste_dominios_cpp feito

--- testes_dominios.cpp
+++ testes_dominios.cpp
@@ -7,18 +7,45 @@ using namespace std;
 int main()
 {
     TesteAvaliacao teste_avaliacao;
-    cout << "AVALIAÇÃO:" << endl << "    ";
+    cout << "AVALIACAO:" << endl << "    ";
     if (teste_avaliacao.run() == TesteAvaliacao::SUCESSO)
-        cout << "Teste 'AVALIAÇÃO' bem-sucedido" << endl;
+        cout << "Teste 'AVALIACAO' bem-sucedido" << endl;
 
+    TesteCodigo teste_codigo;
+    cout << "CODIGO:" << endl << "    ";
+    if (teste_codigo.run() == TesteCodigo::SUCESSO)
+        cout << "Teste 'CODIGO' bem-sucedido" << endl;
+
+    TesteData teste_data;
+    cout << "DATA:" << endl << "    ";
+    if (teste_data.run() == TesteData::SUCESSO)
+        cout << "Teste 'DATA' bem-sucedido" << endl;
+    
+    TesteDinheiro teste_dinheiro;
+    cout << "DATA:" << endl << "    ";
+    if (teste_data.run() == TesteData::SUCESSO)
+        cout << "Teste 'DATA' bem-sucedido" << endl;
+
+    TesteDuracao teste_duracao;
+    cout << "DURACAO:" << endl << "    ";
+    if (teste_dinheiro.run() == TesteDinheiro::SUCESSO)
+        cout << "Teste 'DURACAO' bem-sucedido" << endl;
+
+    TesteHorario teste_horario;
+    cout << "HORÁRIO:" << endl << "    ";
+    if (teste_horario.run() == TesteHorario::SUCESSO)
+        cout << "Teste 'HORÁRIO' bem-sucedido" << endl;
+
+    TesteNome teste_nome;
+    cout << "SENHA:" << endl << "    ";
+    if (teste_nome.run() == TesteNome::SUCESSO)
+        cout << "Teste 'SENHA' bem-sucedido" << endl;
 
     TesteSenha teste_senha;
     cout << "SENHA:" << endl << "    ";
     if (teste_senha.run() == TesteSenha::SUCESSO)
         cout << "Teste 'SENHA' bem-sucedido" << endl;
 
-    TesteHorario teste_horario;
-    cout << "HORÁRIO:" << endl << "    ";
-    if (teste_horario.run() == TesteHorario::SUCESSO)
-        cout << "Teste 'HORÁRIO' bem bem-sucedido" << endl;
+    
+
 }
