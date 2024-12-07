e9f50d51ba3776cf8b43beb2c08a7d3987af419e
Mariarauj <143236678+Mariarauj@users.noreply.github.com>
2024-12-07 18:47:28 -0300

    Update nome.hpp

--- include/dominios/nome.hpp
+++ include/dominios/nome.hpp
@@ -1,3 +1,13 @@
+// Matrícula: 232023969
+
+/**
+ * @file nome.hpp
+ * @brief Declaração da classe 'Nome'.
+ * 
+ * Este arquivo define a classe `Nome`, que valida um título com base em critérios específicos.
+ * 
+ */
+
 #ifndef NOME_HPP
 #define NOME_HPP
 
@@ -5,11 +15,34 @@
 #include <string>
 using namespace std;
 
+/**
+ * @brief Classe responsável pela validação de uma sequência de caracteres usada para autenticação.
+ * 
+ * A sequência de autenticação deve atender aos seguintes requisitos:
+ *
+ * - Texto com até 30 caracteres.
+ * 
+ * Caso algum desses requisitos não seja atendido, será lançada uma exceção do tipo `std::invalid_argument`.
+ * 
+ * @note Esta classe herda os métodos `set` e `get` da classe base `SuperDominio`.
+ * 
+ */
+
 class Nome: public SuperDominio<string>
 {
     private:
+    /**
+     * @brief Valida a sequência fornecida conforme os critérios especificados.
+     * 
+     * Este método verifica se a sequência de autenticação atende aos requisitos de validade.
+     * 
+     * @param T String representando a sequência a ser validada.
+     * 
+     * @throw std::invalid_argument Se a sequência não atender aos critérios.
+     */
+
         void validar(string) override;
 };
 
 
-#endif
+#endif // SENHA_HPP
