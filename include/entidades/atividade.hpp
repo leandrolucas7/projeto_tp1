5490f6b95bba7b03bc428a586284e281c29c8d41
Rafael <rafaelsapienzapinheiro@gmail.com>
2024-12-08 15:36:18 -0300

    Classe "Atividade" documentada

--- include/entidades/atividade.hpp
+++ include/entidades/atividade.hpp
@@ -1,3 +1,15 @@
+//Matrícula: 232006215
+
+/**
+ * @file atividade.hpp
+ * @brief Declaração da classe `Atividade`.
+ * 
+ * Este arquivo define a classe `Atividade`, que armazena informações inerentes ao serviço fornecido ao usuário em um certo destino.
+ * 
+ */
+
+
+
 #ifndef ATIVIDADE_HPP_INCLUDED
 #define ATIVIDADE_HPP_INCLUDED
 
@@ -5,10 +17,15 @@
 
 using namespace std;
 
+/**
+ * @class Atividade
+ * @brief Classe que armazena informações relativas a um serviço provido ao usuário durante uma viagem, em determinado destino.
+ */
+
 class Atividade
 {
     private:
-        Codigo codigo;
+        Codigo codigo; ///< Age como identificador do serviço prestado
         Nome nome;
         Data data;
         Horario horario;
@@ -16,19 +33,75 @@ class Atividade
         Dinheiro preco;
         Avaliacao avaliacao;
     public:
+        /**
+         * @brief Define uma identificação associada ao serviço fornecido.
+         * @param codigo Referência constante para uma instância da classe `Codigo`.
+         */
         void set_codigo(const Codigo&);
-        void set_nome(const Nome&);
-        void set_data(const Data&);
-        void set_horario(const Horario&);
-        void set_duracao(const Duracao&);
-        void set_preco(const Dinheiro&);
-        void set_avaliacao(const Avaliacao&);
+        /**
+         * @brief Retorna a identificação atrelada ao serviço.
+         * @return Instância de `Codigo` .
+         */
         Codigo get_codigo() const;
+        /**
+         * @brief Define um rótulo para serviço fornecido.
+         * @param nome Referência constante para uma instância da classe `Nome`.
+         */
+        void set_nome(const Nome&);
+        /**
+         * @brief Retorna o título do serviço.
+         * @return Instância de `Nome` .
+         */
         Nome get_nome() const;
+        /**
+         * @brief Define um marco temporal, em dia, mês e ano, para serviço fornecido.
+         * @param data Referência constante para uma instância da classe `Data`.
+         */
+        void set_data(const Data&);
+        /**
+         * @brief Retorna o dia em que o serviço foi provido.
+         * @return Instância de `Data`.
+         */
         Data get_data() const;
+        /**
+         * @brief Define o momento do dia, em horas e minutos, em que o serviço ocorreu.
+         * @param horario Referência constante para uma instância da classe `Horario`.
+         */
+        void set_horario(const Horario&);
+        /**
+         * @brief Retorna o horario de execução do serviço.
+         * @return Instância de `Horario`.
+         */
         Horario get_horario() const;
+        /**
+         * @brief Define o príodo de tempo em que o serviço prolongou-se.
+         * @param duracao Referência constante para uma instância da classe `Duracao`.
+         */
+        void set_duracao(const Duracao&);
+        /**
+         * @brief Retorna o período do serviço.
+         * @return Instância de `Duracao`.
+         */
         Duracao get_duracao() const;
+        /**
+         * @brief Define o custo do serviço.
+         * @param preco Referência constante para uma instância da classe `Dinheiro`.
+         */
+        void set_preco(const Dinheiro&);
+        /**
+         * @brief Retorna o custo do serviço.
+         * @return Instância de `Dinheiro`.
+         */
         Dinheiro get_preco() const;
+        /**
+         * @brief Define uma pontuação para o serviço fornecido.
+         * @param avaliacao Referência constante para uma instância da classe `Avaliacao`.
+         */
+        void set_avaliacao(const Avaliacao&);
+        /**
+         * @brief Retorna a nota de satisfação do serviço.
+         * @return Instância de `Avaliacao`.
+         */
         Avaliacao get_avaliacao() const;
 };
 
@@ -37,39 +110,15 @@ inline void Atividade::set_codigo(const Codigo &codigo)
     this->codigo = codigo;
 }
 
-inline void Atividade::set_nome(const Nome &nome)
-{
-    this->nome = nome;
-}
-
-inline void Atividade::set_data(const Data &data)
-{
-    this->data = data;
-}
-
-inline void Atividade::set_horario(const Horario &horario)
-{
-    this->horario = horario;
-}
-
-inline void Atividade::set_duracao(const Duracao &duracao)
-{
-    this->duracao = duracao;
-}
-
-inline void Atividade::set_preco(const Dinheiro &preco)
-{
-    this->preco = preco;
-}
-
-inline void Atividade::set_avaliacao(const Avaliacao &avaliacao)
-{
-    this->avaliacao = avaliacao;
-}
-
 inline Codigo Atividade::get_codigo() const
 {
     return codigo;
+}
+
+
+inline void Atividade::set_nome(const Nome &nome)
+{
+    this->nome = nome;
 }
 
 inline Nome Atividade::get_nome() const
@@ -77,9 +126,19 @@ inline Nome Atividade::get_nome() const
     return nome;
 }
 
+inline void Atividade::set_data(const Data &data)
+{
+    this->data = data;
+}
+
 inline Data Atividade::get_data() const
 {
     return data;
+}
+
+inline void Atividade::set_horario(const Horario &horario)
+{
+    this->horario = horario;
 }
 
 inline Horario Atividade::get_horario() const
@@ -87,14 +146,29 @@ inline Horario Atividade::get_horario() const
     return horario;
 }
 
+inline void Atividade::set_duracao(const Duracao &duracao)
+{
+    this->duracao = duracao;
+}
+
 inline Duracao Atividade::get_duracao() const
 {
     return duracao;
 }
 
+inline void Atividade::set_preco(const Dinheiro &preco)
+{
+    this->preco = preco;
+}
+
 inline Dinheiro Atividade::get_preco() const
 {
     return preco;
+}
+
+inline void Atividade::set_avaliacao(const Avaliacao &avaliacao)
+{
+    this->avaliacao = avaliacao;
 }
 
 inline Avaliacao Atividade::get_avaliacao() const
