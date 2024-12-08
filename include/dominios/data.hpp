d0d7bfd5095106dd8557c0f91220804d4432c949
Rafael <rafaelsapienzapinheiro@gmail.com>
2024-12-08 13:34:12 -0300

    Dinheiro documentado

--- include/dominios/data.hpp
+++ include/dominios/data.hpp
@@ -1,6 +1,6 @@
 // Matrícula: 232006215
 /**
- * @file Data.hpp
+ * @file data.hpp
  * @brief Declaração da classe `Data`.
  * 
  */
@@ -24,19 +24,10 @@ using namespace std;
  * - A string que representa a data deve estar no formato "DD-MM-AA", onde:
  *  - "AA" representa um ano de "00" a "99";
  *  - "MM" representa um mês, de janeiro("01") a dezembro("12");
- *  - "DD" representa um dia. O dia não pode ser um número negativoe o número de dias varia de acordo com o mês. Assim, para que a data seja validada, consideramos que:
-*       - Janeiro possui 31 dias;
-*       - Fevereiro tem 29 dias em anos bissextos e 28 dias nos outros anos;
-*       - Março possui 31 dias;
-*       - Abril possui 30 dias;
-*       - Maio possui 31 dias;
-*       - Junho possui 30 dias;
-*       - Julho possui 31 dias;
-*       - Agosto possui 31 dias;
-*       - Setembro possui 30 dias;
-*       - Outubro possui 31 dias;
-*       - Novembro possui 30 dias;
-*       - Dezembro possui 31 dias;
+ *  - "DD" representa um dia. O dia não pode ser um número negativo e o número de dias varia de acordo com o mês. Assim, para que a data seja validada, consideramos que:
+ *      - O mês de fevereiro tem 29 dias em anos bissextos e 28 dias nos outros anos;
+ *      - Os meses com 30 dias são abril,junho,setembro e novembro;
+ *      - Os meses com 31 dias são janeiro, março, maio, julho, agosto, outubro e dezembro.
 *       .
 *   .
 * .
--- include/dominios/dinheiro.hpp
+++ include/dominios/dinheiro.hpp
@@ -1,9 +1,30 @@
+//Matrícula: 232006215
+
+/**
+ * @file horario.hpp
+ * @brief Declaração da classe `Data`.
+ * 
+ */
+
+
 #ifndef DINHEIRO_HPP
 #define DINHEIRO_HPP
 
 #include "super_dominio.hpp"
 #include <string>
 using namespace std;
+
+/**
+ * @class Dinheiro
+ * @brief Classe que armazena um double que representa valor pecuniário.
+ * 
+ * <b>Regras de formato:</b>
+ * - O valor monetário não pode ser negativo nem exceder 200000;
+ * - O ponto (".") deve ser usado como separador decimal;
+ * - Após o ponto, são aceitos dígitos significativos apenas nas duas primeiras casas decimais. Desse modo, os valores "20", "300.15" e "240.750", por exemplo, seriam validados, ao passo que "1.009", "12.123" e "457.122" seriam rejeitados.
+ * 
+ */
+
 
 class Dinheiro : public SuperDominio<double>
 {
