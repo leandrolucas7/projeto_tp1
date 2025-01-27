55048d51b2bb8836a769d1cb6e827824638a3c14
Leandro Lucas <leandrolucas7@hotmail.com>
2025-01-27 18:56:56 -0300

    estrutura do projeto com bancos de dados

---
+++ include/apresentacao/interfaces_apresentacao.hpp
@@ -0,0 +1,9 @@
+#ifndef INTERFACES_APRESENTACAO_HPP
+#define INTERFACES_APRESENTACAO_HPP
+
+#include "int_apr_autenticacao.hpp"
+#include "int_apr_conta.hpp"
+#include "int_apr_controlador.hpp"
+#include "int_apr_viagem.hpp"
+
+#endif
---
+++ include/servicos/interfaces_servicos.hpp
@@ -0,0 +1,8 @@
+#ifndef INTERFACES_SERVICOS_HPP
+#define INTERFACES_SERVICOS_HPP
+
+#include "int_ser_autenticacao.hpp"
+#include "int_ser_conta.hpp"
+#include "int_ser_viagem.hpp"
+
+#endif
